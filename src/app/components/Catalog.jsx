dcfad5ad3b6e7221fed1e822819d6eba3aef9982
Alex Menendez <amenendez15@usf.edu>
2025-11-01 17:06:12 -0400

    added useeffect fetch in catalog.jsx

---
+++ src/app/components/Catalog.jsx
@@ -0,0 +1,22 @@
+'use client';
+
+import { useEffect, useState } from 'react';
+
+export default function Catalog() {
+  const [products, setProducts] = useState([]);
+
+useEffect(() => {
+    fetch("/api/products")
+    .then(r=> r.json())
+    .then(setProducts)
+    .catc(console.error);
+}, []);
+
+  return (
+    <ul>
+        {products.map(product => (
+            <li key={product.id}>{product.name}:{product.revenue}</li>
+        ))}
+    </ul>   
+    );
+}
